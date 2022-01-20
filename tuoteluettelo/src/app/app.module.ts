7210a481013b5726357a2db4a9ec3c2b5f9407f0
Krista-Mari Katajisto <kristamari.katajisto@gmail.com>
2022-01-20 13:51:38 +0200

    New Components added

--- tuoteluettelo/src/app/app.module.ts
+++ tuoteluettelo/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListViewComponent } from './list-view/list-view.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListViewComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule
