91147807541c0810c24717aae39678da7425496e
Krista-Mari Katajisto <kristamari.katajisto@gmail.com>
2022-01-20 18:21:44 +0200

    changes made

--- tuoteluettelo/src/app/app.module.ts
+++ tuoteluettelo/src/app/app.module.ts
@@ -1,15 +1,15 @@
-import { NgModule } from '@angular/core';
+import { NgModule, OnInit } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { SearchComponent } from './search/search.component';
+
+// import { SearchComponent } from './search/search.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SearchComponent,
-
+    AppComponent
   ],
   imports: [
     BrowserModule
@@ -17,4 +17,5 @@ import { SearchComponent } from './search/search.component';
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
