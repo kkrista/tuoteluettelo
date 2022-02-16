978b21c581a46cfba77ddb64c4cbb20d910e648f
Krista-Mari Katajisto <kristamari.katajisto@gmail.com>
2022-02-16 19:22:25 +0200

    daschboard component created and code added

---
+++ tuoteluettelo/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Phone } from '../interfaces';
+import { PhoneListComponent } from '../phone-list/phone-list.component';
+import { PhonesService } from '../phone.service';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+
+  orderHeader: string = '';
+  phones: Phone[] = [];
+  searchList! : any;
+  objectKeys = Object.keys;
+  items = { keyOne: 'Newest', keyTwo: 'Alphabetical'};
+
+  constructor(private phoneService: PhonesService) { }
+
+  @ViewChild(PhoneListComponent, {static : true}) listComponent : PhoneListComponent | undefined;
+
+  ngOnInit() {
+    this.getPhones();
+  }
+
+  getPhones(): void {
+    this.phoneService.getPhones()
+          .subscribe((phones: Phone[]) => this.phones = phones)
+  }
+
+  sortItem(event: any) {
+    this.listComponent?.sort(event)
+  }
+
+}
