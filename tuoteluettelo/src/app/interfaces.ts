c2e4c8579425e0f1c6a7d7f53b544895f35b23d6
Krista-Mari Katajisto <kristamari.katajisto@gmail.com>
2022-02-16 19:23:28 +0200

    file edited

---
+++ tuoteluettelo/src/app/interfaces.ts
@@ -0,0 +1,8 @@
+export class Phone {
+  id!: String;
+  age!: Number;
+  imageUrl!: String;
+  snippet!: String;
+  name!: String;
+  carrier!: any;
+}
---
+++ tuoteluettelo/src/app/phone.datasource.ts
@@ -0,0 +1,11 @@
+import {Phone} from './interfaces';
+import {} from '../assets/phones/phones.json';
+
+
+
+export const Phones: Phone[] = [
+
+
+
+
+]
