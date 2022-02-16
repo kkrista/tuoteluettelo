f5993cf9ef65139fc553fe78ee234485a6432176
Krista-Mari Katajisto <kristamari.katajisto@gmail.com>
2022-02-16 19:22:52 +0200

    Imports and code added

---
+++ tuoteluettelo/src/app/phone.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { PhoneDetails } from './details.interface';
+import { Phone } from './interfaces';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhonesService {
+
+
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+
+
+  getPhones():Observable<Phone[]> {
+    const phones = this.http.get<Phone[]>("assets/phones/phones.json");
+
+
+    return phones;
+
+  }
+
+  getProduct(id: String): any{
+    const phone = this.http.get<Phone[]>("assets/phones/"+ id +".json");
+    return phone;
+  }
+
+}
